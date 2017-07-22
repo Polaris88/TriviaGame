a284f8d39e5a7f444f8a0d7db194453a2c8aaec4
Jeremy Jackson <JJackson@Jeremys-MacBook-Pro.local>
2017-07-22 12:50:54 -0500

    TriviaGame

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+function submitAnswer() {
+	var total = 8;
+	var score = 0;
+	//Get User Input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+	var q6 = document.forms["quizForm"]["q6"].value;
+	var q7 = document.forms["quizForm"]["q7"].value;
+	var q8 = document.forms["quizForm"]["q8"].value;
+
+	//Validation
+	for (i = 1; i <= total; i++) {
+		if(eval('q' + i) == null || eval('q' + i) == '') {
+			alert('You missed question ' + i);
+			return false;
+		}
+	}
+
+	//Set Correct Answers
+	var answers = ["c", "a", "d", "b", "d", "d", "c", "a"];
+
+	//Check Answers
+	for (i = 1; i <= total; i++) {
+		if (eval('q'+i) === answers[i - 1]){
+			score++;
+		}	
+	}
+	//Display Results
+	var results = document.getElementById('results');
+	results.innerHTML = '<h3>You scored <span>' + score + '</span> out of <span>' + total + '</span></h3>';
+	alert('You scored ' + score + ' out of ' + total);
+
+	return false;
+}
